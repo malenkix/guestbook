3c19537ed3d8b517b3a0a016029db8eb35a6f2e7
Philipp Müller <phil@media-ag.info>
2018-04-13 20:21:40 +0200

    2a : Basics - Pinwall Setup - 01

---
+++ smartbook/js/pinwall.js
@@ -0,0 +1,17 @@
+var lastUpdate;
+
+// will create a new image 3 seconds after site was fully loaded.
+$(function() {
+	var wall = document.getElementById("wall");
+	var top = 250;
+	var left = 667;
+	
+	var pic = document.createElement("img");
+	pic.setAttribute("id", "1024");
+	pic.setAttribute("class", "post");
+	pic.setAttribute("src", "temp/preview.jpg");
+	pic.setAttribute("style", "top:" + top + "px; left:" + left + "px;");
+	
+	
+	setTimeout(function(){ wall.appendChild(pic) }, 3000);
+});
